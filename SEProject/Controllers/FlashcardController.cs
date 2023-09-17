62c79cd2cae496726f9e7913ed1bfe3efb4fb7e1
KonstantinasArelis <arelis.konstantinas@gmail.com>
2023-09-17 22:32:30 +0300

    Flashcard data is read from flashcards.json file

--- SEProject/Controllers/FlashcardController.cs
+++ SEProject/Controllers/FlashcardController.cs
@@ -1,19 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using SEProject.Models;
 
+using System.Text.Json;
+using System.Text.Json.Serialization;
 namespace SEProject.Controllers;
 
 public class FlashcardController : Controller
 {
+    private readonly IWebHostEnvironment _env;
+
+    public FlashcardController(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
     public IActionResult CreateSampleFlashcard()
     {
-        // Create sample flashcards
-        var flashcards = new List<Flashcard>
-        {
-            new Flashcard { question = "What is ASP.NET?", answer = "A web framework" },
-            new Flashcard { question = "What is C#?", answer = "A programming language" },
-            new Flashcard { question = "What is MVC?", answer = "A design pattern" }
-        };
+        // Get the physical path to the JSON file using IWebHostEnvironment
+        string jsonFilePath = System.IO.Path.Combine(_env.ContentRootPath, "flashcards.json");
+        Console.WriteLine($"My Path: {jsonFilePath}");
+        // Read the JSON file
+        string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+
+        List<Flashcard> flashcards = JsonSerializer.Deserialize<List<Flashcard>>(jsonData);
 
         
         return View(flashcards);
