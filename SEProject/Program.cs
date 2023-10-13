30ff3c418b6cf651ff1701a754b016248f9a9f6a
Dominykas Černovas <cernovas50@gmail.com>
2023-10-13 08:38:33 +0300

    Resolving Logger conflict

--- SEProject/Program.cs
+++ SEProject/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IFlashcardDataHandler, FlashcardFileIOService>();
 builder.Services.AddScoped<IFlashcardPackDataHandler, FlashcardPackFileIOService>();
+builder.Services.AddScoped<ILoggingHandler, LoggingService>();
 
 var app = builder.Build();
 
