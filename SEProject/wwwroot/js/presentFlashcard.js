6a22b73fbb9ad814fde77529a6dd6f1ec3591269
Dominykas Černovas <cernovas50@gmail.com>
2023-11-13 17:18:01 +0200

    Question on presenting flashcard stays when 'Reveal' button is pressed

--- SEProject/wwwroot/js/presentFlashcard.js
+++ SEProject/wwwroot/js/presentFlashcard.js
@@ -1,18 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     var revealButton = document.getElementById("revealButton");
-    var arrowButton = document.getElementById("arrowButton");
-    var question = document.getElementById("question");
     var answer = document.getElementById("answer");
 
     answer.style.display = "none";
-    arrowButton.style.display = "none";
 
     revealButton.addEventListener("click", function (event) {
         event.preventDefault();
-        question.style.display = (question.style.display === "none") ? "block" : "none";
         answer.style.display = (answer.style.display === "none") ? "block" : "none";
-        revealButton.style.display = (revealButton.style.display == "none") ? "block" : "none";
-        arrowButton.style.display = (arrowButton.style.display == "none") ? "block" : "none";
+        revealButton.style.display = "none"; // Hide the reveal button
     });
-
-}); 
+});
