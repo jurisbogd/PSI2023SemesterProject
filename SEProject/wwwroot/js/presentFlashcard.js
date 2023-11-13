f221222c56a9d7c86c0626265f8cbc417edeec8c
Rytis Karalius <rytis.karalius321@gmail.com>
2023-11-13 17:51:48 +0200

    Fixed small UI problems

--- SEProject/wwwroot/js/presentFlashcard.js
+++ SEProject/wwwroot/js/presentFlashcard.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     var revealButton = document.getElementById("revealButton");
     var answer = document.getElementById("answer");
+    var arrowButton = document.getElementById("arrowButton");
 
     answer.style.display = "none";
+    arrowButton.style.display = "none";
 
     revealButton.addEventListener("click", function (event) {
         event.preventDefault();
         answer.style.display = (answer.style.display === "none") ? "block" : "none";
-        revealButton.style.display = "none"; // Hide the reveal button
+        revealButton.style.display = (revealButton.style.display == "none") ? "block" : "none";
+        arrowButton.style.display = (arrowButton.style.display == "none") ? "block" : "none";
     });
 });
