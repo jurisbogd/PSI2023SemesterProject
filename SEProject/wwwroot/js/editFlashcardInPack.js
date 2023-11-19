ba791916a1e69e75ec52964770ff5cbbdbaf736c
rytiskar <rytis.karalius321@gmail.com>
2023-11-19 18:38:12 +0200

    Modified flashcard editing view style

--- SEProject/wwwroot/js/editFlashcardInPack.js
+++
@@ -1,7 +0,0 @@
-// JavaScript code to handle the "Edit" button click
-document.querySelectorAll(".edit-button").forEach(function(button) {
-    button.addEventListener("click", function() {
-        var flashcardId = button.getAttribute("data-flashcard-id");
-        window.location.href = "/FlashcardPack/EditFlashcard?flashcardID=" + flashcardId; // Use the correct query parameter name
-    });
-});
