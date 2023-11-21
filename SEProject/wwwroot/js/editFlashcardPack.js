412fbba7e150c416a87a812d93be15d679eee871
Rytis Karalius <rytis.karalius321@gmail.com>
2023-11-21 15:02:04 +0200

    Modified pack editing feature but for some reason text alignment does not work correctly

--- SEProject/wwwroot/js/editFlashcardPack.js
+++ SEProject/wwwroot/js/editFlashcardPack.js
@@ -1,17 +1,49 @@
-    // // JavaScript to show and hide the modal when the "Edit" button is clicked
-    // document.querySelectorAll(".edit-flashcard-pack-button").forEach(function(button) {
-    //     button.addEventListener("click", function() {
-    //         var modalId = "editModal-" + button.getAttribute("data-flashcardPack-id");
-    //         var modal = document.getElementById(modalId);
-    //         modal.style.display = "block";
-    //     });
-    // });
+document.addEventListener("DOMContentLoaded", function () {
+    var editButtons = document.querySelectorAll(".edit-flashcard-pack-button");
 
-    // JavaScript to show and hide the modal when the "Edit" button is clicked
-document.querySelectorAll(".edit-flashcard-pack-button").forEach(function(button) {
-    button.addEventListener("click", function() {
-        var modalId = "editModal-" + button.getAttribute("data-flashcardPack-id");
-        var modal = document.getElementById(modalId);
-        modal.style.display = "block";
+    editButtons.forEach(function (editButton) {
+        var flashcardPackId = editButton.getAttribute('data-flashcardPack-id');
+        var saveButton = document.querySelector(".save-button[data-flashcardPack-id='" + flashcardPackId + "']");
+        var cancelButton = document.querySelector(".cancel-button[data-flashcardPack-id='" + flashcardPackId + "']");
+        var textarea = document.getElementById('pack-' + flashcardPackId);
+
+        editButton.addEventListener("click", function () {
+
+            textarea.readOnly = false;
+
+            textarea.focus();
+            textarea.classList.add("editable");
+
+            editButton.style.display = "none";
+            saveButton.style.display = "inline-block";
+            cancelButton.style.display = "inline-block";
+        });
+
+        saveButton.addEventListener("click", function () {
+
+            textarea.classList.remove("editable");
+
+            var updatedValue = textarea.value;
+
+            updatePackOnServer(updatedValue);
+
+            textarea.readOnly = true;
+            editButton.style.display = "inline-block";
+            saveButton.style.display = "none";
+            cancelButton.style.display = "none";
+        });
+
+        function updatePackOnServer(value) {
+            $.ajax({
+                type: "POST",
+                url: 'EditFlashcardPackName',
+                data: { id: flashcardPackId, newName: value},
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                dataType: "json",
+                success: function (result) {
+                    console.log("Success")
+                }
+            });
+        }
     });
 });
