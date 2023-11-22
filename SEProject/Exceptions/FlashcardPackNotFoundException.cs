f3920e2b6a65aa8bc9c6d4ed221f7dcdfb8c8c3f
jurisbogd <juris.bogdanas@gmail.com>
2023-11-22 04:14:05 +0200

    Modified FlashcardPackNotFoundException.
    Added additional constructors, changed formatting, removed unnecessary using statement.

--- SEProject/Exceptions/FlashcardPackNotFoundException.cs
+++ SEProject/Exceptions/FlashcardPackNotFoundException.cs
@@ -1,11 +1,7 @@
-using System;
-
-namespace SEProject.Exceptions
-{
-    public class FlashcardPackNotFoundException : Exception
-    {
-        public FlashcardPackNotFoundException(string message) : base(message)
-        {
-        }
+namespace SEProject.Exceptions {
+    public class FlashcardPackNotFoundException : Exception {
+        public FlashcardPackNotFoundException() : base() {}
+        public FlashcardPackNotFoundException(string message) : base(message) {}
+        public FlashcardPackNotFoundException(string message, Exception inner) : base(message, inner) {}
     }
 }
