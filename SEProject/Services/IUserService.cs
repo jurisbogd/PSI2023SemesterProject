3151099784918b99bfb36bb6cefa83decf4080b1
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-12-04 12:59:11 +0200

    Create UserService and IUserService, change User class a bit to be able to implement it in the UserService

---
+++ SEProject/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using SEProject.Models;
+
+namespace SEProject.Services
+{
+    public interface IUserService
+    {
+/*        public void CreateNewUser(string username, string email, string password);*/
+    }
+}
---
+++ SEProject/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using SEProject.Models;
+
+namespace SEProject.Services
+{
+    public class UserService : IUserService
+    {
+/*        public void CreateNewUser(string username, string email, string password)
+        {
+            User newUser = new User();
+        }*/
+    }
+}
