16fb90f8f8f912bd9a2f958ce9cf7d3adaa11580
Dominykas Černovas <cernovas50@gmail.com>
2023-12-05 11:19:01 +0200

    Every user can create his own packs

--- SEProject/Services/FlashcardPackIOService.cs
+++ SEProject/Services/FlashcardPackIOService.cs
@@ -16,12 +16,23 @@ public class FlashcardPackIOService : IFlashcardPackDataHandler
         this._context = context;
     }
 
-    public async Task<FlashcardPack>? LoadFlashcardPackAsync(Guid ID)
+    public async Task<FlashcardPack>? LoadFlashcardPackAsync(Guid packId, Guid userID)
     {
+        // Check if the user has access to the specified pack
+        bool userHasAccess = await _context.UserFlashcardPacks
+            .AnyAsync(ufp => ufp.UserID == userID && ufp.PackID == packId);
+
+        if (!userHasAccess)
+        {
+            // Handle the case where the user doesn't have access to the pack
+            throw new Exception("User does not have access to the specified FlashcardPack");
+        }
+
         FlashcardPack? flashcardPack = await _context.FlashcardPacks
             .Include(pack => pack.Flashcards)
-            .FirstOrDefaultAsync(pack => pack.ID == ID);
-         if (flashcardPack == null)
+            .FirstOrDefaultAsync(pack => pack.ID == packId);
+
+        if (flashcardPack == null)
         {
             // Handle the case where the flashcardPack is null
             throw new Exception("FlashcardPack not found");
@@ -30,34 +41,50 @@ public class FlashcardPackIOService : IFlashcardPackDataHandler
         return flashcardPack;
     }
 
-    public async Task<List<FlashcardPack>> LoadFlashcardPacksAsync()
+    public async Task<List<FlashcardPack>> LoadFlashcardPacksAsync(Guid userID)
     {
+        // Check if the user has access to any flashcard packs
+        List<Guid> accessiblePackIds = await _context.UserFlashcardPacks
+            .Where(ufp => ufp.UserID == userID)
+            .Select(ufp => ufp.PackID)
+            .ToListAsync();
+
         List<FlashcardPack> flashcardPacks = await _context.FlashcardPacks
-        .Include(pack => pack.Flashcards)
-        .ToListAsync();
+            .Where(pack => accessiblePackIds.Contains(pack.ID))
+            .Include(pack => pack.Flashcards)
+            .ToListAsync();
 
         return flashcardPacks;
     }
 
-    public async Task SaveFlashcardPackAsync(FlashcardPack flashcardPack, Func<FlashcardPack, bool> validationFunction = null)
+    public async Task SaveFlashcardPackAsync(FlashcardPack flashcardPack, Guid userId, Func<FlashcardPack, bool> validationFunction = null)
     {
         if (validationFunction != null && !validationFunction(flashcardPack))
         {
             // Validation failed, do not save the flashcard
             throw new Exception("Validation failed: FlashcardPack is invalid");
         }
+
         var existingPack = await _context.FlashcardPacks
             .FirstOrDefaultAsync(pack => pack.ID == flashcardPack.ID);
 
         if (existingPack == null)
         {
             _context.FlashcardPacks.Add(flashcardPack);
+
+            // Create a new entry in UserFlashcardPacks for the user and the added flashcard pack
+            var userFlashcardPack = new UserFlashcardPacks
+            {
+                UserID = userId,
+                PackID = flashcardPack.ID
+            };
+            _context.UserFlashcardPacks.Add(userFlashcardPack);
         }
         else
         {
             existingPack.Name = flashcardPack.Name;
         }
-        
+
         await _context.SaveChangesAsync();
         OnFlashcardPackChanged(new FlashcardPackEventArgs(flashcardPack, "Saved"));
     }
@@ -65,13 +92,22 @@ public class FlashcardPackIOService : IFlashcardPackDataHandler
     public async Task RemoveFlashcardPackAsync(Guid ID)
     {
         var packToDelete = await _context.FlashcardPacks
-        .Include(pack => pack.Flashcards)
-        .FirstOrDefaultAsync(pack => pack.ID == ID);
+            .Include(pack => pack.Flashcards)
+            .FirstOrDefaultAsync(pack => pack.ID == ID);
 
         if (packToDelete == null)
         {
             // Handle the case where the flashcardPack is null
             throw new FlashcardPackNotFoundException($"FlashcardPack with ID {ID} not found");
+        }
+
+        // Remove the associated entry in UserFlashcardPacks
+        var userFlashcardPackToDelete = await _context.UserFlashcardPacks
+            .FirstOrDefaultAsync(ufp => ufp.PackID == ID);
+
+        if (userFlashcardPackToDelete != null)
+        {
+            _context.UserFlashcardPacks.Remove(userFlashcardPackToDelete);
         }
 
         _context.FlashcardPacks.Remove(packToDelete!);
@@ -80,6 +116,7 @@ public class FlashcardPackIOService : IFlashcardPackDataHandler
         await _context.SaveChangesAsync();
         OnFlashcardPackChanged(new FlashcardPackEventArgs(packToDelete, "Deleted"));
     }
+
 
     public virtual void OnFlashcardPackChanged(FlashcardPackEventArgs e)
     {
