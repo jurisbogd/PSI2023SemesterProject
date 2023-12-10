81ed8c3aeaffb5a72ce25ebf5ae4a99cd6945f5a
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-12-10 11:56:28 +0200

    Change simple log interceptor to check if the email exists in the database

--- DatabaseAPI/DatabaseAPI/Interceptors/LoggingInterceptor.cs
+++ DatabaseAPI/DatabaseAPI/Interceptors/LoggingInterceptor.cs
@@ -1,22 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using DatabaseAPI.Models;
+using DatabaseAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DatabaseAPI.Interceptors
 {
     public class LoggingInterceptor : IAsyncActionFilter
     {
+        private readonly IUserService _userService;
+
+        public LoggingInterceptor(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            // Check if the email already exists in the database
+            var model = context.ActionArguments["model"] as CreateUserModel;
+            if (model != null)
+            {
+                var existingUser = await _userService.FindUserByEmailAsync(model.email);
+                if (existingUser != null)
+                {
+                    Console.WriteLine($"Email {model.email} already exists in the database. Action aborted.");
+                    //context.Result = new BadRequestObjectResult("Email already exists");
+                    return;
+                }
+            }
+
             // Log the start of the action
             Console.WriteLine($"Action {context.ActionDescriptor.DisplayName} started at {DateTime.Now}");
-
-            // Continue with the action execution
             var resultContext = await next();
 
             // Log the end of the action
             Console.WriteLine($"Action {context.ActionDescriptor.DisplayName} completed at {DateTime.Now}");
-
-            // If you want to log the result, you can access it using resultContext.Result
-            // For example, you can log the status code:
             Console.WriteLine($"Status code: {resultContext.HttpContext.Response.StatusCode}");
         }
     }
